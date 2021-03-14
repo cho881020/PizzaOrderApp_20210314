578622171552023fc26fc392b7765ff8863847b2
조경진 <cho881020@gmail.com>
2021-03-14 12:08:39 +0900

    어댑터 / 리스트뷰 연결까지 진행

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/fragments/PizzaStoresFragment.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/fragments/PizzaStoresFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.tjoeun.pizzaorderapp_20210314.R
+import com.tjoeun.pizzaorderapp_20210314.adapters.StoreAdapter
 import com.tjoeun.pizzaorderapp_20210314.datas.Store
+import kotlinx.android.synthetic.main.fragment_pizza_stores.*
 
 class PizzaStoresFragment : Fragment() {
 
     val mPizzaStores = ArrayList<Store>()
+
+    lateinit var mPizzaStoreAdapter : StoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +31,10 @@ class PizzaStoresFragment : Fragment() {
         mPizzaStores.add(Store("파파존스", "1577-8080", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"))
         mPizzaStores.add(Store("미스터피자", "1577-0077", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"))
         mPizzaStores.add(Store("도미노피자", "1577-3082", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"))
+
+        mPizzaStoreAdapter = StoreAdapter(context!!, R.layout.store_list_item, mPizzaStores)
+
+        pizzaStoreListView.adapter = mPizzaStoreAdapter
 
     }
 
