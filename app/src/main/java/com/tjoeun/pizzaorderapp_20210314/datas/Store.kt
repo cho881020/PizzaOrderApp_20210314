6fa7a7b14eea2a34b9422920b15833051f810183
조경진 <cho881020@gmail.com>
2021-03-14 12:45:16 +0900

    가게 클릭시 클릭된 데이터를 들고 => 상세화면으로 이동  - UI에는 반영 안한 상태.

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/datas/Store.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/datas/Store.kt
@@ -1,7 +1,9 @@
 package com.tjoeun.pizzaorderapp_20210314.datas
 
+import java.io.Serializable
+
 class Store(
     val storeName : String,
     val phoneNum : String,
-    val logoUrl : String) {
+    val logoUrl : String) : Serializable {
 }
